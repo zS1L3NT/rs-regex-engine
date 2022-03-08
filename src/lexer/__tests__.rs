64ba5127b4749df0cda9a90b64f55f90bc6c8c7d
Zechariah <zechariahtan144@gmail.com>
2022-03-08 11:44:35 +0800

    Modified tests and added a few more

--- src/lexer/__tests__.rs
+++ src/lexer/__tests__.rs
@@ -9,6 +9,28 @@ macro_rules! equals {
             false
         })
     };
+    ($a:literal, Ok, $count:literal) => {
+        assert_eq!(
+            if let Ok(tokens) = Lexer::new($a.to_string()).lex() {
+                tokens.len()
+            } else {
+                println!("\"{}\" was not Ok", $a);
+                0
+            },
+            $count
+        )
+    };
+    ($a:literal, Ok, $tokens:expr) => {
+        assert_eq!(
+            if let Ok(tokens) = Lexer::new($a.to_string()).lex() {
+                tokens
+            } else {
+                println!("\"{}\" was not Ok", $a);
+                vec![]
+            },
+            $tokens
+        )
+    };
     ($a:literal, Err) => {
         assert!(if let Err(_) = Lexer::new($a.to_string()).lex() {
             true
@@ -17,28 +39,39 @@ macro_rules! equals {
             false
         })
     };
+    ($a:literal, Err, $msg:literal) => {
+        assert!(if let Err(err) = Lexer::new($a.to_string()).lex() {
+            err.msg.starts_with($msg)
+        } else {
+            println!("\"{}\" was not Err", $a);
+            false
+        })
+    };
 }
 
 #[test]
-fn valid_regex_start() {
+fn regex_start() {
     equals!("/abc/", Ok);
     equals!("/bcd/", Ok);
-}
-
-#[test]
-fn invalid_regex_start() {
     equals!("abc", Err);
     equals!("bcd", Err);
 }
 
 #[test]
-fn valid_regex_end() {
+fn regex_end() {
     equals!("/abc/", Ok);
     equals!("/bcd/", Ok);
+    equals!("abc", Err);
+    equals!("bcd", Err);
 }
 
 #[test]
-fn invalid_regex_end() {
-    equals!("abc", Err);
-    equals!("bcd", Err);
+fn length_check() {
+    equals!("//", Err);
+}
+
+#[test]
+fn valid_all_literals() {
+    equals!("/abc/", Ok, 3);
+    equals!("/abc/", Ok, 3);
 }
