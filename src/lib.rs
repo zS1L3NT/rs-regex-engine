8d4989d068fddba90255c4905c74bb084813d0be
Zechariah <zechariahtan144@gmail.com>
2022-03-11 03:03:52 +0800

    Added opsult enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod error;
+mod opsult;
 mod regex;
 mod traits;
 
 pub use error::Error;
+pub use opsult::Opsult;
 pub use regex::RegExp;
 pub use traits::MatchesRegExp;
 pub use traits::ToRegExp;
