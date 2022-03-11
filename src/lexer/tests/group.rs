09dd05fe9219d53fee35a49ece1b5370cec0a020
Zechariah <zechariahtan144@gmail.com>
2022-03-12 02:15:24 +0800

    TOO - MUCH - REFACTORING

--- src/lexer/tests/group.rs
+++ src/lexer/tests/group.rs
@@ -1,149 +1,246 @@
-use super::*;
-use crate::{invalid, valid};
+mod invalid {
+    use super::super::*;
+    use crate::invalid;
 
-#[test]
-fn groups() {
-    invalid!("/(/", "Incomplete group structure");
-    invalid!("/)/", "Unmatched parenthesis");
-    invalid!("/(?/", "Incomplete group structure");
+    #[test]
+    fn start() {
+        invalid!("/(/", "Incomplete group structure");
+    }
+
+    #[test]
+    fn end() {
+        invalid!("/)/", "Unmatched parenthesis");
+    }
+
+    #[test]
+    fn start_with_question_mark() {
+        invalid!("/(?/", "Incomplete group structure");
+    }
 }
 
-#[test]
-fn group_capturing() {
-    valid!("/()/", vec![OpenGroup(Capturing, 0), CloseGroup(1)]);
-    valid!(
-        "/(abc)/",
-        vec![
-            OpenGroup(Capturing, 0),
-            Literal('a', 1),
-            Literal('b', 2),
-            Literal('c', 3),
-            CloseGroup(4)
-        ]
-    );
+mod capturing {
+    use super::super::*;
+    use crate::valid;
+
+    #[test]
+    fn empty() {
+        valid!("/()/", vec![OpenGroup(Capturing, 0), CloseGroup(1)]);
+    }
+
+    #[test]
+    fn with_literal() {
+        valid!(
+            "/(a)/",
+            vec![OpenGroup(Capturing, 0), Literal('a', 1), CloseGroup(2)]
+        );
+    }
 }
 
-#[test]
-fn group_non_capturing() {
-    invalid!("/(?:/", "Incomplete group structure");
-    valid!("/(?:)/", vec![OpenGroup(NonCapturing, 0), CloseGroup(3)]);
-    valid!(
-        "/(:?)/",
-        vec![
-            OpenGroup(Capturing, 0),
-            Literal(':', 1),
-            Quantifier(QuestionMark, 2),
-            CloseGroup(3)
-        ]
-    );
-    valid!(
-        "/(?:x)/",
-        vec![OpenGroup(NonCapturing, 0), Literal('x', 3), CloseGroup(4)]
-    );
+mod non_capturing {
+    use super::super::*;
+    use crate::{invalid, valid};
+
+    #[test]
+    fn invalid() {
+        invalid!("/(?:/", "Incomplete group structure");
+    }
+
+    #[test]
+    fn empty() {
+        valid!("/(?:)/", vec![OpenGroup(NonCapturing, 0), CloseGroup(3)]);
+    }
+
+    #[test]
+    fn as_literals() {
+        valid!(
+            "/(:?)/",
+            vec![
+                OpenGroup(Capturing, 0),
+                Literal(':', 1),
+                Quantifier(QuestionMark, 2),
+                CloseGroup(3)
+            ]
+        );
+    }
+
+    #[test]
+    fn with_literal() {
+        valid!(
+            "/(?:x)/",
+            vec![OpenGroup(NonCapturing, 0), Literal('x', 3), CloseGroup(4)]
+        );
+    }
 }
 
-#[test]
-fn group_positive_lookahead() {
-    invalid!("/(?=/", "Incomplete group structure");
-    valid!(
-        "/(?=)/",
-        vec![OpenGroup(PositiveLookAhead, 0), CloseGroup(3)]
-    );
-    valid!(
-        "/(=?)/",
-        vec![
-            OpenGroup(Capturing, 0),
-            Literal('=', 1),
-            Quantifier(QuestionMark, 2),
-            CloseGroup(3)
-        ]
-    );
-    valid!(
-        "/(?=x)/",
-        vec![
-            OpenGroup(PositiveLookAhead, 0),
-            Literal('x', 3),
-            CloseGroup(4)
-        ]
-    );
+mod positive_lookahead {
+    use super::super::*;
+    use crate::{invalid, valid};
+
+    #[test]
+    fn invalid() {
+        invalid!("/(?=/", "Incomplete group structure");
+    }
+
+    #[test]
+    fn empty() {
+        valid!(
+            "/(?=)/",
+            vec![OpenGroup(PositiveLookAhead, 0), CloseGroup(3)]
+        );
+    }
+
+    #[test]
+    fn as_literals() {
+        valid!(
+            "/(=?)/",
+            vec![
+                OpenGroup(Capturing, 0),
+                Literal('=', 1),
+                Quantifier(QuestionMark, 2),
+                CloseGroup(3)
+            ]
+        );
+    }
+
+    #[test]
+    fn with_literal() {
+        valid!(
+            "/(?=x)/",
+            vec![
+                OpenGroup(PositiveLookAhead, 0),
+                Literal('x', 3),
+                CloseGroup(4)
+            ]
+        );
+    }
 }
 
-#[test]
-fn group_positive_lookbehind() {
-    invalid!("/(?</", "Incomplete group structure");
-    invalid!("/(?<=/", "Incomplete group structure");
-    valid!(
-        "/(?<=)/",
-        vec![OpenGroup(PositiveLookBehind, 0), CloseGroup(4)]
-    );
-    valid!(
-        "/(?<=x)/",
-        vec![
-            OpenGroup(PositiveLookBehind, 0),
-            Literal('x', 4),
-            CloseGroup(5)
-        ]
-    );
-    valid!(
-        "/(?=<x)/",
-        vec![
-            OpenGroup(PositiveLookAhead, 0),
-            Literal('<', 3),
-            Literal('x', 4),
-            CloseGroup(5)
-        ]
-    );
+mod positive_lookbehind {
+    use super::super::*;
+    use crate::{invalid, valid};
+
+    #[test]
+    fn invalid() {
+        invalid!("/(?</", "Incomplete group structure");
+        invalid!("/(?<=/", "Incomplete group structure");
+    }
+
+    #[test]
+    fn empty() {
+        valid!(
+            "/(?<=)/",
+            vec![OpenGroup(PositiveLookBehind, 0), CloseGroup(4)]
+        );
+    }
+
+    #[test]
+    fn as_literals() {
+        valid!(
+            "/(?<=x)/",
+            vec![
+                OpenGroup(PositiveLookBehind, 0),
+                Literal('x', 4),
+                CloseGroup(5)
+            ]
+        );
+    }
+
+    #[test]
+    fn with_literal() {
+        valid!(
+            "/(?=<x)/",
+            vec![
+                OpenGroup(PositiveLookAhead, 0),
+                Literal('<', 3),
+                Literal('x', 4),
+                CloseGroup(5)
+            ]
+        );
+    }
 }
 
-#[test]
-fn group_negative_lookahead() {
-    invalid!("/(?!/", "Incomplete group structure");
-    valid!(
-        "/(?!)/",
-        vec![OpenGroup(PositiveLookAhead, 0), CloseGroup(3)]
-    );
-    valid!(
-        "/(!?)/",
-        vec![
-            OpenGroup(Capturing, 0),
-            Literal('!', 1),
-            Quantifier(QuestionMark, 2),
-            CloseGroup(3)
-        ]
-    );
-    valid!(
-        "/(?!x)/",
-        vec![
-            OpenGroup(PositiveLookAhead, 0),
-            Literal('x', 3),
-            CloseGroup(4)
-        ]
-    );
+mod negative_lookahead {
+    use super::super::*;
+    use crate::{invalid, valid};
+
+    #[test]
+    fn invalid() {
+        invalid!("/(?!/", "Incomplete group structure");
+    }
+
+    #[test]
+    fn empty() {
+        valid!(
+            "/(?!)/",
+            vec![OpenGroup(PositiveLookAhead, 0), CloseGroup(3)]
+        );
+    }
+
+    #[test]
+    fn as_literals() {
+        valid!(
+            "/(!?)/",
+            vec![
+                OpenGroup(Capturing, 0),
+                Literal('!', 1),
+                Quantifier(QuestionMark, 2),
+                CloseGroup(3)
+            ]
+        );
+    }
+
+    #[test]
+    fn with_literal() {
+        valid!(
+            "/(?!x)/",
+            vec![
+                OpenGroup(PositiveLookAhead, 0),
+                Literal('x', 3),
+                CloseGroup(4)
+            ]
+        );
+    }
 }
 
-#[test]
-fn group_negative_lookbehind() {
-    invalid!("/(?</", "Incomplete group structure");
-    invalid!("/(?<!/", "Incomplete group structure");
-    valid!(
-        "/(?<!)/",
-        vec![OpenGroup(NegativeLookBehind, 0), CloseGroup(4)]
-    );
-    valid!(
-        "/(?<!x)/",
-        vec![
-            OpenGroup(NegativeLookBehind, 0),
-            Literal('x', 4),
-            CloseGroup(5)
-        ]
-    );
-    valid!(
-        "/(?!<x)/",
-        vec![
-            OpenGroup(NegativeLookAhead, 0),
-            Literal('<', 3),
-            Literal('x', 4),
-            CloseGroup(5)
-        ]
-    );
+mod negative_lookbehind {
+    use super::super::*;
+    use crate::{invalid, valid};
+
+    #[test]
+    fn invalid() {
+        invalid!("/(?</", "Incomplete group structure");
+        invalid!("/(?<!/", "Incomplete group structure");
+    }
+
+    #[test]
+    fn empty() {
+        valid!(
+            "/(?<!)/",
+            vec![OpenGroup(NegativeLookBehind, 0), CloseGroup(4)]
+        );
+    }
+
+    #[test]
+    fn as_literals() {
+        valid!(
+            "/(?<!x)/",
+            vec![
+                OpenGroup(NegativeLookBehind, 0),
+                Literal('x', 4),
+                CloseGroup(5)
+            ]
+        );
+    }
+    #[test]
+    fn with_literal() {
+        valid!(
+            "/(?!<x)/",
+            vec![
+                OpenGroup(NegativeLookAhead, 0),
+                Literal('<', 3),
+                Literal('x', 4),
+                CloseGroup(5)
+            ]
+        );
+    }
 }
