256e40e4d6b509258f01e5792f9cac7fd194180b
Zechariah <zechariahtan144@gmail.com>
2022-03-11 22:35:00 +0800

    Implemented a bit of the lexer

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,38 +1,87 @@
-use crate::error::Error;
-
-use super::Token;
+use crate::{
+    lexer::{Pos, Token},
+    Error,
+    Opsult::{self, Err as _Err, Some as _Some},
+};
 
 pub struct Lexer {
-    string: String,
+    chars: Vec<char>,
+    tokens: Vec<Token>,
+    pos: Pos,
 }
 
 impl Lexer {
     pub fn new(string: String) -> Self {
-        Self { string }
+        Self {
+            chars: string.chars().collect(),
+            tokens: vec![],
+            pos: 0,
+        }
     }
 
-    pub fn lex(&self) -> Result<Vec<Token>, Error> {
-        let mut string = self.string.clone();
-        let mut tokens = vec![];
-
-        if string.starts_with("/") {
-            string.remove(0);
-        } else {
-            return Err(Error::new(
-                "Expected RegExp to start with a </>".to_string(),
-                0,
-            ));
+    pub fn lex(&mut self) -> Opsult<Vec<Token>, Error> {
+        if let Some(first_char) = self.chars.get(0) {
+            if *first_char != '/' {
+                return _Err(Error::new(
+                    "Expected RegExp to start with a </>".to_string(),
+                    0,
+                ));
+            }
         }
 
-        if string.ends_with("/") {
-            string.remove(string.len() - 1);
-        } else {
-            return Err(Error::new(
-                "Expected RegExp to end with a </>".to_string(),
-                string.len() - 1,
-            ));
+        if let Some(last_char) = self.chars.last() {
+            if *last_char != '/' {
+                return _Err(Error::new(
+                    "Expected RegExp to end with a </>".to_string(),
+                    self.chars.len() - 1,
+                ));
+            }
         }
 
-        Ok(tokens)
+        self.chars.remove(0);
+        self.chars.remove(self.chars.len() - 1);
+
+        while self.chars.len() > 0 {
+            if self.lex_anchors() {
+                continue;
+            }
+
+            if self.lex_brackets() {
+                continue;
+            }
+
+            if let Some(char) = self.chars.first() {
+                self.tokens.push(Token::Literal(*char, self.pos));
+                self.chars.remove(0);
+                self.pos += 1;
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        _Some(vec![])
+    }
+
+    fn lex_anchors(&mut self) -> bool {
+        match self.chars.first() {
+            Some('^') => {
+                self.tokens.push(Token::AnchorStart(self.pos));
+                self.chars.remove(0);
+                self.pos += 1;
+                true
+            }
+            Some('$') => {
+                let token = Token::AnchorEnd(self.pos);
+                self.chars.remove(0);
+                self.pos += 1;
+                true
+            }
+            _ => false,
+        }
+    }
+
+    fn lex_brackets(&self) -> bool {
+        false
     }
 }
--- src/lexer/tests/mod.rs
+++ src/lexer/tests/mod.rs
@@ -7,19 +7,21 @@ mod regex;
 mod special;
 
 pub use super::{
-    token::{OpenBracket, OpenGroup, Quantifier, Special, Token},
+    super::Opsult,
+    token::{
+        OpenBracket::{self, *},
+        OpenGroup::{self, *},
+        Quantifier::{self, *},
+        Special::{self, *},
+        Token::{self, *},
+    },
     Lexer,
 };
-pub use OpenBracket::*;
-pub use OpenGroup::*;
-pub use Quantifier::*;
-pub use Special::*;
-pub use Token::*;
 
 #[macro_export]
 macro_rules! valid {
     ($a:literal, $count:literal) => {
-        let count = if let Ok(tokens) = Lexer::new($a.to_string()).lex() {
+        let count = if let Opsult::Some(tokens) = Lexer::new($a.to_string()).lex() {
             tokens.len()
         } else {
             println!("\"{}\" was not Ok", $a);
@@ -28,7 +30,7 @@ macro_rules! valid {
         assert_eq!(count, $count);
     };
     ($a:literal, $tokens:expr) => {
-        let tokens = if let Ok(tokens) = Lexer::new($a.to_string()).lex() {
+        let tokens = if let Opsult::Some(tokens) = Lexer::new($a.to_string()).lex() {
             tokens
         } else {
             println!("\"{}\" was not Ok", $a);
@@ -41,7 +43,7 @@ macro_rules! valid {
 #[macro_export]
 macro_rules! invalid {
     ($a:literal, $msg:literal) => {
-        let msg_starts_with = if let Err(err) = Lexer::new($a.to_string()).lex() {
+        let msg_starts_with = if let Opsult::Err(err) = Lexer::new($a.to_string()).lex() {
             err.msg.starts_with($msg)
         } else {
             println!("\"{}\" was not Err", $a);
