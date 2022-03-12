8dffa5531f252734886aba67d35850d7d0bd5167
Zechariah <zechariahtan144@gmail.com>
2022-03-13 03:04:36 +0800

    Added basic nodes and setup parser

---
+++ src/parser/node.rs
@@ -0,0 +1,68 @@
+use std::fmt::Debug;
+
+trait Node: NodeClone + Debug {}
+
+trait NodeClone {
+    fn clone_box(&self) -> Box<dyn Node>;
+}
+
+impl<T: 'static + Node + Clone> NodeClone for T {
+    fn clone_box(&self) -> Box<dyn Node> {
+        Box::new(self.clone())
+    }
+}
+
+impl Clone for Box<dyn Node> {
+    fn clone(&self) -> Self {
+        self.clone_box()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Quantity {
+    OneOrMore,
+    ZeroOrMore,
+    ZeroOrOne,
+    Count(usize),
+    Range(usize, Option<usize>),
+}
+
+#[derive(Debug, Clone)]
+pub struct SingleNode {
+    value: char,
+    quantity: Quantity,
+}
+
+#[derive(Debug, Clone)]
+pub struct MultiNode {
+    variant: MultiVariant,
+    values: Vec<Box<dyn Node>>,
+    quantity: Quantity,
+}
+
+#[derive(Debug, Clone)]
+pub enum MultiVariant {
+    AND,
+    OR,
+    NAND,
+}
+
+#[derive(Debug, Clone)]
+pub struct GroupNode {
+    variant: GroupVariant,
+    quantity: Quantity,
+}
+
+#[derive(Debug, Clone)]
+pub enum GroupVariant {
+    Capturing,
+    NonCapturing,
+    PositiveLookAhead,
+    PositiveLookBehind,
+    NegativeLookAhead,
+    NegativeLookBehind,
+}
+
+impl Node for SingleNode {}
+impl Node for MultiNode {}
+impl Node for GroupNode {}
