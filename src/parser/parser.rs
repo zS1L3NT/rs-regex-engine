caf0f3446101c65d6bf2fc58ac83550b7dbfd72f
Zechariah <zechariahtan144@gmail.com>
2022-03-14 02:38:14 +0800

    Added basic Parser struct and impl

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,1 +1,15 @@
-pub struct Parser {}
+use super::{super::lexer::Token, *};
+
+pub struct Parser {
+    tokens: Vec<Token>,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Self { tokens }
+    }
+
+    fn parse() -> Option<Node> {
+        None
+    }
+}
